e6d91221cd527a98313509de135a160f1e534c63
Giorgio Bar <giorgio.bar@gmail.com>
2021-03-16 16:35:29 +0100

    ignore node_modules in current folder and any subfolders

--- ex_0/ex_0.js
+++ ex_0/ex_0.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/**
+ * Given an array of strings, for each string computes a new string
+ * made of the first two and the last two characters of the original string.
+ *  The new string should replace the old one in the same array.
+ * @param {array} strings
+ */
 function firstAndLastTwo(strings) {
     for( let i=0; i<strings.length; i++) {
         const len = strings[i].length;
@@ -21,4 +27,4 @@ firstAndLastTwo(originalStrings);
 
 console.log(originalStrings);
 
-debugger;
+// debugger;
